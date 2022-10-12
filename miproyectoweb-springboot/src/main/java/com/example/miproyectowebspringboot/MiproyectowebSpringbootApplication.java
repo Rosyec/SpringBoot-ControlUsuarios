5338a8b6baa0993ed7fcc458462b5c74cbd0551a
Rosyec <chechomens@gmail.com>
2022-10-12 15:14:18 -0500

    Creación del proyecto con sus respectivas dependencias

---
+++ miproyectoweb-springboot/src/main/java/com/example/miproyectowebspringboot/MiproyectowebSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.miproyectowebspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiproyectowebSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiproyectowebSpringbootApplication.class, args);
+	}
+
+}
