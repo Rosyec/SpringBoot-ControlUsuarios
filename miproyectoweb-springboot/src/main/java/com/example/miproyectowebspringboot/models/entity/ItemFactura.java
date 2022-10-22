9335ec43ab55a6412f14a5005cdec8293e97c327
Rosyec <chechomens@gmail.com>
2022-10-22 12:21:20 -0500

    Creaion y relacion de las Entity ItemFactura y Producto

---
+++ miproyectoweb-springboot/src/main/java/com/example/miproyectowebspringboot/models/entity/ItemFactura.java
@@ -0,0 +1,51 @@
+package com.example.miproyectowebspringboot.models.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "item_facturas")
+public class ItemFactura implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer cantidad;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "producto_id")
+    private Producto producto;
+
+    public ItemFactura() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Double calcularImporte() {
+        return this.cantidad.doubleValue() * this.producto.getPrecio();
+    }
+}
