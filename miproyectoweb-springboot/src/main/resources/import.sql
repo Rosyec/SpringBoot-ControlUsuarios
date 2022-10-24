f2575a5f3c587af33e38da3d42c1331ad657c66f
Rosyec <chechomens@gmail.com>
2022-10-24 14:23:07 -0500

    Se agregó la consulta Ajax que permite cargar los productos

--- miproyectoweb-springboot/src/main/resources/import.sql
+++ miproyectoweb-springboot/src/main/resources/import.sql
@@ -4,6 +4,8 @@ INSERT INTO clientes (nombre, apellido, email, create_at, foto) VALUES('Jim', 'P
 INSERT INTO productos (nombre, precio, create_at) VALUES('TV Panasonic', 150000, NOW());
 INSERT INTO productos (nombre, precio, create_at) VALUES('Camara Sony', 39000, NOW());
 INSERT INTO productos (nombre, precio, create_at) VALUES('Xbox Series S', 43600, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Xbox Series X', 55000, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Smartphone Samsung Galaxy A52S', 122000, NOW());
 
 INSERT INTO facturas (descripcion, observacion, create_at, cliente_id) VALUES('Factura de equipos de oficina', 'Efectivo', NOW(), 1);
 INSERT INTO item_facturas (cantidad, producto_id, factura_id) VALUES(1, 1, 1);
