55991b736670fcc3e5b93d4273169a90469ddcb8
Rosyec <chechomens@gmail.com>
2022-10-25 17:57:43 -0500

    Agregando la validación de logeo

--- miproyectoweb-springboot/src/main/java/com/example/miproyectowebspringboot/controllers/LoginController.java
+++ miproyectoweb-springboot/src/main/java/com/example/miproyectowebspringboot/controllers/LoginController.java
@@ -5,17 +5,24 @@ import java.security.Principal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
-    @GetMapping("/login")
 
-    public String login(Model model, Principal principal, RedirectAttributes flash){
+    @GetMapping("/login")
+    public String login(@RequestParam(value = "error", required = false) String error, Model model, Principal principal, RedirectAttributes flash){
+
         if (principal != null) {
             flash.addFlashAttribute("info", "Ya has iniciado sesión");
-            return "redirec:/";
+            return "redirect:/app/";
         }
+
+        if (error != null) {
+            model.addAttribute("error", "Error: Nombre de usuario o contraseña incorrecta!");
+        }
+
         return "login";
     }
 }
