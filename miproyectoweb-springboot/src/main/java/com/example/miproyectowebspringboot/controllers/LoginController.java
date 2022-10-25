b7d23b81301a8f1b33cd32ad33719b3ee0a23827
Rosyec <chechomens@gmail.com>
2022-10-25 18:44:41 -0500

    Implementación de un menu para login y logout

--- miproyectoweb-springboot/src/main/java/com/example/miproyectowebspringboot/controllers/LoginController.java
+++ miproyectoweb-springboot/src/main/java/com/example/miproyectowebspringboot/controllers/LoginController.java
@@ -12,7 +12,9 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class LoginController {
 
     @GetMapping("/login")
-    public String login(@RequestParam(value = "error", required = false) String error, Model model, Principal principal, RedirectAttributes flash){
+    public String login(@RequestParam(value = "error", required = false) String error,
+    @RequestParam(value = "logout", required = false) String logout
+    , Model model, Principal principal, RedirectAttributes flash){
 
         if (principal != null) {
             flash.addFlashAttribute("info", "Ya has iniciado sesión");
@@ -21,6 +23,10 @@ public class LoginController {
 
         if (error != null) {
             model.addAttribute("error", "Error: Nombre de usuario o contraseña incorrecta!");
+        }
+
+        if (logout != null) {
+            model.addAttribute("success", "Has cerrado sesión exitosamente!");
         }
 
         return "login";
